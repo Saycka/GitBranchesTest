9fda67fe60fbb3514545c0af2c922aff4377703e
Andrey <a>
2019-03-31 18:00:54 +0300

    common commit 2 foo()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    foo();
+  }
+
+  public static void foo() {
+    System.out.println("master branch");
   }
 
 }

c656e842b5793fd0877d72bab29b689b14eccee0
Andrey <a>
2019-03-31 18:01:36 +0300

    branch1 commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello, World");
+    foo1();
+  }
+
+  public static void foo1() {
+    System.out.println("branch1");
   }
 
 }
